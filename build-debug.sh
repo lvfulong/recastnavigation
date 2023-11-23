e36339fb88492f28520ba1703b42032ddaa3b1c1
helloworldlv <775331175@qq.com>
2023-11-23 17:20:45 +0800

    fuck

--- build-debug.sh
+++ build-debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf wasm_build
-cmake .  -B ./build/debug -DTARGET_BUILD_PLATFORM=emscripten -DCMAKE_TOOLCHAIN_FILE=~/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake --no-warn-unused-cli -DCMAKE_INSTALL_PREFIX=./install/emscripten/recastnavigation -DRECASTNAVIGATION_EXAMPLES=OFF -DRECASTNAVIGATION_TESTS=OFF -DRECASTNAVIGATION_DEMO=OFF -DCMAKE_BUILD_TYPE=debug -DCMAKE_CROSSCOMPILING_EMULATOR=/home/ubuntu/emsdk/node/16.20.0_64bit/bin/node
+cmake ./EmscriptenBindings  -B ./build/debug -DTARGET_BUILD_PLATFORM=emscripten -DCMAKE_TOOLCHAIN_FILE=~/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake --no-warn-unused-cli -DCMAKE_INSTALL_PREFIX=./install/emscripten/recastnavigation -DRECASTNAVIGATION_EXAMPLES=OFF -DRECASTNAVIGATION_TESTS=OFF -DRECASTNAVIGATION_DEMO=OFF -DCMAKE_BUILD_TYPE=debug -DCMAKE_CROSSCOMPILING_EMULATOR=/home/ubuntu/emsdk/node/16.20.0_64bit/bin/node
 cd ./build/debug
 emmake make -j4
 make install
