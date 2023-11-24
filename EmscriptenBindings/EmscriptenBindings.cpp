bedf71ccb6f7fca7351b84b6221dd62d16f2110d
helloworldlv <775331175@qq.com>
2023-11-24 15:27:07 +0800

    lerp

--- EmscriptenBindings/EmscriptenBindings.cpp
+++ EmscriptenBindings/EmscriptenBindings.cpp
@@ -5,8 +5,11 @@
 
 using namespace emscripten;
 
-
+float lerp(float a, float b, float t) {
+    return (1 - t) * a + t * b;
+}
 EMSCRIPTEN_BINDINGS(recastnavigation) {
     constant("TEST", 9999);
+	function("lerp", &lerp);
 }
 
