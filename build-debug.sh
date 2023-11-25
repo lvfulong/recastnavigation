e60367c8d031eaf04f709bcd8bd96146d788adb0
helloworldlv <775331175@qq.com>
2023-11-25 15:43:51 +0800

    cp

--- build-debug.sh
+++ build-debug.sh
@@ -4,4 +4,6 @@ mkdir -p dist
 cmake ./EmscriptenBindings  -B ./build/debug -DTARGET_BUILD_PLATFORM=emscripten -DCMAKE_TOOLCHAIN_FILE=~/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake --no-warn-unused-cli -DCMAKE_INSTALL_PREFIX=./dist  -DCMAKE_BUILD_TYPE=debug -DCMAKE_CROSSCOMPILING_EMULATOR=/home/ubuntu/emsdk/node/16.20.0_64bit/bin/node
 cd ./build/debug
 emmake make -j4
-make install
+cd ../../dist
+cp ../build/debug/recast-navigation.js .
+cp ../build/debug/recast-navigation.wasm .
--- build-release.sh
+++ build-release.sh
@@ -4,4 +4,6 @@ mkdir -p dist
 cmake ./EmscriptenBindings  -B ./build/release -DTARGET_BUILD_PLATFORM=emscripten -DCMAKE_TOOLCHAIN_FILE=~/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake --no-warn-unused-cli -DCMAKE_INSTALL_PREFIX=./dist  -DCMAKE_BUILD_TYPE=release -DCMAKE_CROSSCOMPILING_EMULATOR=/home/ubuntu/emsdk/node/16.20.0_64bit/bin/node
 cd ./build/release
 emmake make -j4
-make install
+cd ../../dist
+cp ../build/release/recast-navigation.js .
+cp ../build/release/recast-navigation.wasm .
