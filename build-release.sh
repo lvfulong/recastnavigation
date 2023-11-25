26459dda55701edb287bd286776b38b66c091951
helloworldlv <775331175@qq.com>
2023-11-25 15:34:20 +0800

    cc

--- build-release.sh
+++ build-release.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-
 mkdir -p ./build
 mkdir -p dist
-
-
-emcmake cmake -B build -S EmscriptenBindings -DCMAKE_BUILD_TYPE=Release
-cmake --build build
-
-
-
-cp ./build/recast-navigation.* ./dist/
+cmake ./EmscriptenBindings  -B ./build/release -DTARGET_BUILD_PLATFORM=emscripten -DCMAKE_TOOLCHAIN_FILE=~/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake --no-warn-unused-cli -DCMAKE_INSTALL_PREFIX=./dist  -DCMAKE_BUILD_TYPE=release -DCMAKE_CROSSCOMPILING_EMULATOR=/home/ubuntu/emsdk/node/16.20.0_64bit/bin/node
+cd ./build/release
+emmake make -j4
